e8036cbaeda67dc21bf2b97c2470c56572f79b69
Maria Shevchenko <shevtma@yandex.ru>
2022-10-30 23:55:41 +0300

    Fixed some mistakes

--- calculations.js
+++ calculations.js
@@ -3,9 +3,9 @@ function increaseBalance(x, y) {
   return x;
 }
 
-function decreaseBalance(x) {
-x = x - y;
-return x;
+function decreaseBalance(x, y) {
+  x = x - y;
+  return x;
 }
 
 function divideBalanceByAccounts(x, y) {
@@ -21,5 +21,5 @@ function getRestAfterDivision(x, y) {
 
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
-console.log(divideBalanceByAccounts(3000, 2, 4000));
+console.log(divideBalanceByAccounts(3000, 2));
 console.log(getRestAfterDivision(7000, 3));
